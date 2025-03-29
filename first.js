4b369d8ee44761da3709996458aace4a926b13ba
Piyush singh <piyushsingh@gmail.com>
2025-03-29 15:06:32 +0530

    first

--- first.js
+++ first.js
@@ -5,41 +5,41 @@
 // weather:
 
 
-//  const Promises = fetch(`http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=London&aqi=yes`)
-// // console.log(Promises)
+ const Promises = fetch(`http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=London&aqi=yes`)
+// console.log(Promises)
 
-//  .then(response=>response.json())
-//  .then(data=>console.log(data.current.temp_f))       // temp change hota rahega
-//  .catch((error)=>console.log(error));
+ .then(response=>response.json())
+ .then(data=>console.log(data.current.temp_f))       // temp change hota rahega
+ .catch((error)=>console.log(error));
 
-//   console.log(Promises);
+  console.log(Promises);
 
 
-  // Promises.then((response)=>{
+  Promises.then((response)=>{
 
-  //       response.json();
-  //       console.log(response.json());
+        response.json();
+        console.log(response.json());
 
-  //      const pro2 = response.json();
-  //      pro2.then((data)=>{
-  //       console.log(data);               // store all information
-  //      })
+       const pro2 = response.json();
+       pro2.then((data)=>{
+        console.log(data);               // store all information
+       })
          
-  //       return response.json();
-  //   })
+        return response.json();
+    })
 
 
 
-// Promises.then((response)=>{
-//     console.log(response);
-// }).catch((error)=>{
-//    console.log(error);
-// })
+Promises.then((response)=>{
+    console.log(response);
+}).catch((error)=>{
+   console.log(error);
+})
 
 
-// setTimeout(()=>{
-//     console.log(Promises);
-// },2000);
+setTimeout(()=>{
+    console.log(Promises);
+},2000);
 
 
 // three state of promise
