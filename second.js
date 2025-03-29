675c67d89ed570b90fb00df90dfe74057cd483c6
Piyush singh <piyushsingh@gmail.com>
2025-03-29 15:04:07 +0530

    first

--- second.js
+++ second.js
@@ -1,25 +1,25 @@
 
-// document.querySelector('button').addEventListener('click',()=>{
+document.querySelector('button').addEventListener('click',()=>{
 
-//    const place =  document.getElementById('location').value;
+   const place =  document.getElementById('location').value;
    
-//    function updateTemp(data){
-//       const  element = document.getElementById('weatherInfo');
-//       element.innerHTML = `Today's Temperature: ${data.current.temp_c}`;
-//    }
+   function updateTemp(data){
+      const  element = document.getElementById('weatherInfo');
+      element.innerHTML = `Today's Temperature: ${data.current.temp_c}`;
+   }
 
-//    const prom = fetch(`http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=${place}&aqi=yes`)
+   const prom = fetch(`http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=${place}&aqi=yes`)
  
-//    prom
-//    .then(response=>response.json())
-//    .then(data=> updateTemp(data));       
+   prom
+   .then(response=>response.json())
+   .then(data=> updateTemp(data));       
    
-// })   
+})   
 
 
 
 
-console.log("data");
-console.log("piyush");
-console.log("Rajkumar")
+// console.log("data");
+// console.log("piyush");
+// console.log("Rajkumar")
 // console.log();
