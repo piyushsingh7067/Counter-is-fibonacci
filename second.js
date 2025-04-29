e6c0f2f9c3597e0a0f46259d558ffe8ef8f76740
Piyush singh <piyushsingh@gmail.com>
2025-04-29 12:59:06 +0530

    last

--- second.js
+++ second.js
@@ -8,8 +8,8 @@ document.querySelector('button').addEventListener('click',()=>{
       element.innerHTML = `Today's Temperature: ${data.current.temp_c}`;
    }
 
-   const prom = fetch(`http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=${place}&aqi=yes`)
- 
+   const prom = fetch(` http://api.weatherapi.com/v1/current.json?key=2185b42cb04e41d6ae075514252001&q=${place}&aqi=yes`)
+  
    prom
    .then(response=>response.json())
    .then(data=> updateTemp(data));       
